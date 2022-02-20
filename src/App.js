4f95403b2b2ef4ea98327b0b1941b3a7d139e4b4
Salvador Oliveira <slsalvadorlucas@gmail.com>
2022-02-20 17:54:50 -0400

    add profile links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
     <div className="App">
       <h1>Salvador Oliveira</h1>
+      <p>Software Engineer</p>
+      <div>
+        <a href="https://github.com/Salv4d" alt="Github" target="_blank">
+          <FontAwesomeIcon className="App-faIcon" icon={faGithub} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/salvador-oliveira/"
+          alt="LinkedIn"
+          target="_blank"
+        >
+          <FontAwesomeIcon className="App-faIcon" icon={faLinkedin} />
+        </a>
+        <a href="https://twitter.com/Salv4d" alt="Twitter" target="_blank">
+          <FontAwesomeIcon className="App-faIcon" icon={faTwitter} />
+        </a>
+      </div>
     </div>
   );
 }
